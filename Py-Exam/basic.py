fecc041be8f53e66ccb7f8a4c18c02134cac810d
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-10-24 11:48:05 +0200

    exam i am commintg

--- Py-Exam/basic.py
+++ Py-Exam/basic.py
@@ -83,3 +83,81 @@ print('length:', len(text))
 
 txt = 'the quick brown fox jumps over the lazy dog.'
 print( 'brown' in txt)
+
+if 'fox' in txt:
+    print('yes fox in the line')
+    
+if 'rabit' not in txt:
+    print('rabit is not here.')
+    
+print(txt[0:5])
+print(txt[:6])
+print(txt[2:])
+print(txt[-5:-3])
+
+print(txt.upper())
+print(txt.lower())
+print(txt.strip())
+
+txxt = 'replacing'
+print(txxt.replace("r","R"))
+print(txt.split(' '))
+
+#f-string
+age = 24
+textt = f'my name is jack and i am {age}'
+print(textt)
+#using f string  i can put the number inside the text.
+
+price = 59
+des = f'the price of the product is {price:.2f} dollar'
+print(des)
+
+multiplication = f'multiplication is: {5*5}'
+print(multiplication)
+
+
+#escape chachacter
+character = "we are the so-called \"Vikings\" from the north"
+print(character)
+
+
+#boolean
+print(bool('hello'))
+print(bool(12))
+print(bool(''))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(()))
+print(bool({}))
+print(bool([]))
+
+def myFunction():
+    return True
+
+if myFunction():
+    print('it return True')
+else:
+    print('it return false')
+    
+#instance
+k = 200
+print(isinstance(k,int))
+
+
+#Arithmatic math by python
+u = 5
+v = 6
+
+print('py arithmatic math: ',u+v)
+print('py arithmatic math: ',u-v)
+print('py arithmatic math: ',u*v)
+print('py arithmatic math: ',u/v)
+print('py arithmatic math: ',u%v)
+print('py arithmatic math: ',u**v)
+print('py arithmatic math: ',u//y)
+
+
+#Assignment Operators
