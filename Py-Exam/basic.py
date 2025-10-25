5869d7c4aa1d35b2085d76954533f84b9d677857
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-10-25 22:06:57 +0200

    set is easy

--- Py-Exam/basic.py
+++ Py-Exam/basic.py
@@ -328,7 +328,13 @@ while i<len(color):
 
 #set
 
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
 
+myset = set1.union(set2, set3, set4)
+print(myset)
 
 
 
