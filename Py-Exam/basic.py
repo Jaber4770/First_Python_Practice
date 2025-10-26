66241d7d4d3ac0108c45cb6245501b612a1e4e60
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-10-26 21:50:17 +0100

    python else if

--- Py-Exam/basic.py
+++ Py-Exam/basic.py
@@ -389,5 +389,20 @@ if val1>val2:
 else:
     print("he botsho tmi mittha bolecho.")
 
-
+#elif:
+day = 3
+if day ==1:
+    print('monday')
+elif day ==2:
+    print('tuesday')
+elif day ==3:
+    print('wednesday')
+elif day ==4:
+    print("thursday")
+elif day == 5:
+    print("friday'")
+elif day == 6:
+    print('saturday')
+elif day ==7:
+    print('sunday')
 
