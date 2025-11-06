25995398d2b5282c629c9bbcd061ff5a0dc73ff7
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-11-06 21:27:11 +0100

    finding 2nd highest and 2nd lowest doneg

--- exam2_0.py/scnd.py
+++ exam2_0.py/scnd.py
@@ -1,2 +1,25 @@
 #2. **Find Second Lowest and Second Highest**
 #   Given a list of numbers, print 2nd smallest and 2nd largest elements.
+def findHighest(lst):
+    highest = lst[0]
+    lowest = lst[0]
+    secondLargest = None
+    secondLowest = None
+    for x in lst:
+        if highest < x:
+            highest = x
+        if lowest > x:
+            lowest = x
+    for x in lst:
+        if x < highest:
+            if secondLargest is None or x > secondLargest:
+                secondLargest = x
+        if x > lowest:
+            if secondLowest is None or x < secondLowest:
+                secondLowest = x
+    
+    print("Second Largest: ", secondLargest)
+    print("Second Lowest: ", secondLowest)
+
+lst = [10,20,30,40,50,60,70,88,90,80,110]
+findHighest(lst)
