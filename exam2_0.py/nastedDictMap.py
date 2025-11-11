f4ed99334cd761cf0224e43672c2f463afa3da73
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-11-11 22:42:51 +0100

    nested dict maping to regular dict

--- exam2_0.py/nastedDictMap.py
+++ exam2_0.py/nastedDictMap.py
@@ -5,4 +5,12 @@
 #    Output: {"A":"Dhaka","B":"Dhaka","C":"Chittagong"}
 #    ```
 #
+data = {"Dhaka": ["A", "B"], "Chittagong": ["C"]}
+theDict = {}
+for x in data:
+    if x not in theDict:
+        for y in data[x]:
+            theDict[y] = x
 
+
+print(theDict)
