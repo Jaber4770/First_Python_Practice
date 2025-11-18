ea9ff06f250731ba9e1bd1cce84df0634742c59d
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-11-19 00:25:33 +0100

    find common from 2 list

--- exam3.py/findCommon.py
+++ exam3.py/findCommon.py
@@ -3,3 +3,10 @@
 
 list1 = [1,2,3,4,5,6]
 list2 = [4,5,6,7,8,9,10]
+common = []
+for x in list1:
+    for y in list2:
+        if x == y:
+            common.append(x)
+            
+print("here is common elemnet from both list: ",common)
