1665444080b38d41785ff118cf601099eb06b05e
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-11-23 14:10:14 +0100

    basic calculation

---
+++ beforeTheExam/unime.py
@@ -0,0 +1,40 @@
+# **Q1 — What is the output of the following program?**
+ 
+x = 5
+y = 3
+x = x + y
+y = x - y
+print(x, y)
+
+# **Q2 — What does this program print?**
+#python
+a = 1
+b = 1
+for i in range(4):
+   a = a + b
+   b = a - b
+print(a)
+
+
+#**Q3 — Rewrite this code using a while loop instead of for:**
+for i in range(1, 11):
+   print(i)
+# --------
+i = 1
+while range(i, 11):
+    print(i)
+    i+= 1
+
+
+#**Q4 — What is the value of variable `s` at the end?**
+s = 0
+for i in range(1, 6):
+   s = s + i*i
+print(s)
+
+# **Q5 — What is the output?**
+
+x = 10
+while x > 0:
+   x = x - 3
+   print("biyogfol",x)
