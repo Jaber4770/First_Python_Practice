3bd9922c499db37ef194e0bdbf2ceb46283c5b79
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-12-05 16:41:26 +0100

    modified

--- beforeTheExam/reverse.py
+++ beforeTheExam/reverse.py
@@ -1,9 +1,9 @@
-txt = input("write the message to revese: ")
+txt = input("write the message to reverse: ")
 reversedTxt = ''
 reversList = []
 for i in range(len(txt)-1, -1, -1):
     reversList.append(txt[i])
     
 reversedTxt = ''.join(reversList)
-print("ei nen revese: ",reversedTxt)
+print("ei nen reverse: ",reversedTxt)
 print("kam shesh ostad!")
