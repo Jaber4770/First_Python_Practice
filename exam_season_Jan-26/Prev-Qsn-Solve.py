61d4bf3f8cc8e219dda2ef84fccd562e31467500
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2026-01-10 14:16:01 +0100

    solving previous questions

--- exam_season_Jan-26/Prev-Qsn-Solve.py
+++ exam_season_Jan-26/Prev-Qsn-Solve.py
@@ -38,8 +38,48 @@ print(result)
 
 # ------------------------------------------------
 
-# ------------------------------------------------
+def count_case(s):
+    upper = 0
+    lower = 0
+    for x in s:
+        if x.isupper():
+            upper +=1
+        elif x.islower():
+            lower +=1
+    return (upper, lower)
+
+text = "Hello"
+result = count_case(text)
+print(result)
 
 # ------------------------------------------------
 
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
+
+
+result = factorial(5)
+print('factorial :', result)
+        
 # ------------------------------------------------
+for i in range(3):
+    if i == 2:
+        break
+else:
+    print('Done')
+# ------------------------------------------------
+
+def common_elements(a,b):
+    result = a
+    for x in b:
+        if x not in result:
+            result.append(x)
+    return result
+
+list1 = [1,2,3,4]
+list2 = [1,3,4,5,6,7,8,9,0]
+removeDuplicate = common_elements(list1, list2)
+print("removed duplicate: ", removeDuplicate)
+        
