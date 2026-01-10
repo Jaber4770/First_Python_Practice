63c3d15fb6ed991713ef9b56bc85760b0834afe5
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2026-01-10 14:41:16 +0100

    solved 4 marks question

--- exam_season_Jan-26/Prev-Qsn-Solve.py
+++ exam_season_Jan-26/Prev-Qsn-Solve.py
@@ -21,9 +21,10 @@ def merge_dicts(d1, d2):
     for x in d1:
         for y in d2:
             if x == y:
-                result[x] = d1[x] + d2[y]  
+                result[x] = d1[x] + d2[y]
     return result
-        
+
+
 d1 = {
     "a": [1, 2, 3],
     "b": [0, 9, 8, 7, 6]
@@ -38,21 +39,24 @@ print(result)
 
 # ------------------------------------------------
 
+
 def count_case(s):
     upper = 0
     lower = 0
     for x in s:
         if x.isupper():
-            upper +=1
+            upper += 1
         elif x.islower():
-            lower +=1
+            lower += 1
     return (upper, lower)
+
 
 text = "Hello"
 result = count_case(text)
 print(result)
 
 # ------------------------------------------------
+
 
 def factorial(n):
     if n == 0 or n == 1:
@@ -62,7 +66,7 @@ def factorial(n):
 
 result = factorial(5)
 print('factorial :', result)
-        
+
 # ------------------------------------------------
 for i in range(3):
     if i == 2:
@@ -71,15 +75,33 @@ else:
     print('Done')
 # ------------------------------------------------
 
-def common_elements(a,b):
+
+def remove_duplicate(a, b):
     result = a
     for x in b:
         if x not in result:
             result.append(x)
     return result
 
-list1 = [1,2,3,4]
-list2 = [1,3,4,5,6,7,8,9,0]
-removeDuplicate = common_elements(list1, list2)
+
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [1, 3, 4, 5, 6, 7, 8, 9, 0]
+removeDuplicate = remove_duplicate(list1, list2)
 print("removed duplicate: ", removeDuplicate)
-        
+
+# ------------------------------------------------
+
+
+def common_element(a, b):
+    common = []
+    for x in a:
+        for y in b:
+            if x == y and x not in common:
+                common.append(x)
+    return common
+
+
+lst1 = [1, 1, 2, 3, 4, 5, 6]
+lst2 = [1, 2, 3, 4, 6, 7, 8, 9, 0]
+commontResult = common_element(lst1, lst2)
+print(commontResult)
