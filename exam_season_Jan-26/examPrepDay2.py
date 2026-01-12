86a286dfb5f89a8813cff883cc7f871ea6b924bc
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2026-01-12 09:41:58 +0100

    finding the max from the inner list whose list contain odd number of elements

--- exam_season_Jan-26/examPrepDay2.py
+++ exam_season_Jan-26/examPrepDay2.py
@@ -1,20 +1,22 @@
-i = 0 
+i = 0
 while i < 10:
     if i % 2 == 0:
         i += 1
         continue
     print(i)
     i += 1
-    
-#---------------------------------------------------------------------------
 
-my_list = [1,2,3,4,5,6,7,8]
+# ---------------------------------------------------------------------------
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
 for num in my_list:
     if num % 2 == 0:
         print("num: ", num)
-        
-#---------------------------------------------------------------------------
-#finding only odd num
+
+# ---------------------------------------------------------------------------
+# finding only odd num
+
+
 def onlyodd(xlist):
     oddNum = []
     for x in xlist:
@@ -22,7 +24,8 @@ def onlyodd(xlist):
             oddNum.append(x)
     return oddNum
 
-numbers = [1,12,3,14,5,16,17,8,19,4]
+
+numbers = [1, 12, 3, 14, 5, 16, 17, 8, 19, 4]
 result = onlyodd(numbers)
 print(result)
 
@@ -33,16 +36,18 @@ if x < 10:
     print("x is less than 10")
 else:
     print('x is greater than 10')
-    
+
 # ---------------------------------------------------------------------------
+
 
 def digit_sum(n):
     sum = 0
-    
+
     if n == 0:
         return 0
     else:
         return (n % 10) + digit_sum(n // 10)
+
 
 numb = 123
 sumResult = digit_sum(numb)
@@ -51,10 +56,13 @@ print('recursively sum: ', sumResult)
 # ---------------------------------------------------------------------------
 
 # apply discount
+
+
 def apply_discount(price, discount_percent):
     discount_price = price - (price * discount_percent/100)
     rounded_discount_price = round(discount_price, 2)
     return rounded_discount_price
+
 
 price = 100
 discount_parcent = 10
@@ -62,6 +70,40 @@ discount = apply_discount(price, discount_parcent)
 print('discount: ', discount)
 
 # ---------------------------------------------------------------------------
+# reverse text
 
+
+def reverse():
+    text = input("write a sentece: ")
+    reverseOrdered = text[-1::-1]
+    print(reverseOrdered)
+
+
+reverse()
 
 # ---------------------------------------------------------------------------
+
+a = 0
+if a == 0:
+    a = 1
+if a == 1:
+    a = 2
+else:
+    a = 3
+print(a)
+
+# ---------------------
+"""question: a data structure consists of a list of lists. The inner lists, whose number is not  known in advance, contain a variable number of numeric elements. For example: xs = [[1,7,12], [4,2,10,14], [15,3,2,11,20]] . implement the python function ds_oddmax(xlist) that returns a list containg the maximum elements contained in the inner lists havings an odd number of element. this questions requires that exactly only the function must be implemented. therefore no data structure nor function all are requested from the candidate."""
+
+
+def ds_oddmax(xlist):
+    resultList = []
+    for x in xlist:
+        if len(x) % 2 == 1:
+            resultList.append(max(x))
+    return resultList
+
+
+xs = [[1, 7, 12], [4, 2, 10, 14], [15, 3, 2, 11, 20]]
+result = ds_oddmax(xs)
+print(result)
