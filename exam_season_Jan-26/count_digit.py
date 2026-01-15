50c80a07ab5f66ae27b63d3456acda8623d5c77e
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2026-01-15 07:33:41 +0100

    merwith dict with matching key

--- exam_season_Jan-26/count_digit.py
+++ exam_season_Jan-26/count_digit.py
@@ -13,3 +13,27 @@ def count_digits(start, end):
 
 count_digit_result = count_digits(100, 105)
 print(count_digit_result)
+
+# ---------------------------------------
+# merge dicts
+
+
+def merge_dicts(d1, d2):
+    result = {} 
+    for x in d1:
+        if x in d2:  # time complexity O(n)
+            result[x] = d1[x] + d2[x]
+    return result
+"""     for x in d1:
+        for y in d2:
+            if x == y: 
+                result[x] = d1[x] + d2[y] 
+time complexity O(n2)
+                """
+
+
+d1 = {"a": [1], "b": [2], "c": [3,4]}
+d2 = {"a": [3], "b": [4], "d": [3,4]}
+
+res = merge_dicts(d1, d2)
+print("result: ", res)
