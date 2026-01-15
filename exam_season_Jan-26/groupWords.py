293bb26dbf981584ee9d8d969cd4a4fdbebd67b1
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2026-01-15 10:11:54 +0100

    string list problem sovled

--- exam_season_Jan-26/groupWords.py
+++ exam_season_Jan-26/groupWords.py
@@ -12,6 +12,23 @@ def group_words(words):
     return groupWords
 
 
-text = "the quick brown fox jumps over the laxy dog"
+text = "the quick brown fox jumps over the lazy dog"
 resullt = group_words(text.split())
 print(resullt)
+
+
+# -----------------------------------------------------
+#question: write the Python function stringlist() that asks the user to input some strings and returns the list containing all the strings. the string 'stop' is used to exit theinput phase and it must be excluded from the list returned.
+
+def stringlist():
+    text = input("enter text & 'stop' to exit: ")
+    result = []
+
+    for x in text.split():
+        if x == 'stop':
+            break
+        result.append(x)
+    return result
+
+stringResult = stringlist()
+print(stringResult)
